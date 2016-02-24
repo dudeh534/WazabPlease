e5d09a8b4ef5c3027a82ab5baaff17380977d648
dufmaakedl@gmail.com <dufmaakedl@naver.com>
2016-02-25 01:21:29 +0900

    apply list

--- app/src/main/java/com/ourincheon/wazap/Require/ListAdapter.java
+++ app/src/main/java/com/ourincheon/wazap/Require/ListAdapter.java
@@ -1,6 +1,8 @@
 package com.ourincheon.wazap.Require;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,25 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
 import com.ourincheon.wazap.R;
+import com.ourincheon.wazap.Recycler_item;
+import com.ourincheon.wazap.Retrofit.ContestData;
+import com.ourincheon.wazap.Retrofit.Contests;
+import com.ourincheon.wazap.WazapService;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.GsonConverterFactory;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 /**
  * Created by Youngdo on 2016-02-18.
@@ -19,11 +39,17 @@ public class ListAdapter extends ArrayAdapter<String> {
     TextView dday, title, cate, man;
     ImageView small;
     Button bt;
+    Contests contest;
+    ArrayList<ContestData> cont_list;
+    int count;
 
     public ListAdapter(Activity context, int resource) {
         super(context, resource);
         this.context = context;
-
+        //list = new ArrayList<Contests>();
+        SharedPreferences pref = context.getSharedPreferences("pref", context.MODE_PRIVATE);
+        String access_token = pref.getString("access_token","");
+        loadPage(access_token);
     }
 
     @Override
@@ -48,4 +74,75 @@ public class ListAdapter extends ArrayAdapter<String> {
     public int getCount() {
         return 5;
     }
+
+    void loadPage(String access_token)
+    {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://come.n.get.us.to/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        WazapService service = retrofit.create(WazapService.class);
+
+
+
+        Call<Contests> call = service.getAppplylist(access_token, 10, 10);
+        call.enqueue(new Callback<Contests>() {
+            @Override
+            public void onResponse(Response<Contests> response) {
+                if (response.isSuccess() && response.body() != null) {
+
+                    Log.d("SUCCESS", response.message());
+                    contest = response.body();
+
+                    //user = response.body();
+                    Log.d("SUCCESS", contest.getMsg());
+
+                   String result = new Gson().toJson(contest);
+                   Log.d("SUCESS-----", result);
+
+                   JSONObject jsonRes;
+                    try {
+                        jsonRes = new JSONObject(result);
+                        JSONArray jsonArr = jsonRes.getJSONArray("data");
+                        count = jsonArr.length();
+                        for(int i =0; i<count; i++) {
+                            ContestData con = new ContestData();
+                            con.setTitle(jsonArr.getJSONObject(i).getString("title"));
+                            con.setCategories(jsonArr.getJSONObject(i).getString("categories"));
+                            con.setPeriod(jsonArr.getJSONObject(i).getString("period"));
+                            con.setRecruitment(jsonArr.getJSONObject(i).getString("recruitment"));
+                            cont_list.add(jsonArr.getJSONObject(0));
+                        }
+                      //  list.add()
+                        /*int len = jsonArr.length();
+                        item = new Recycler_item[len];
+                        id_list = new String[len];
+
+                        for(int i =0 ; i<len; i++) {
+                            id_list[i]=jsonArr.getJSONObject(i).getString("contests_id");
+                            item[i] = new Recycler_item(jsonArr.getJSONObject(i).getString("title"), jsonArr.getJSONObject(i).getString("username"), jsonArr.getJSONObject(i).getString("hosts"));
+                            items.add(item[i]);
+                            //
+                            System.out.println(items.get(i).getName());
+                        }
+                        rec.notifyDataSetChanged();*/
+                    } catch (JSONException e) {
+                    }
+
+                } else if (response.isSuccess()) {
+                    Log.d("Response Body isNull", response.message());
+                } else {
+                    Log.d("Response Error Body", response.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                t.printStackTrace();
+                Log.e("Error", t.getMessage());
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/ourincheon/wazap/Retrofit/ContestData.java
+++ app/src/main/java/com/ourincheon/wazap/Retrofit/ContestData.java
@@ -6,10 +6,11 @@ package com.ourincheon.wazap.Retrofit;
 
 public class ContestData {
     int contests_id;
+    int recruitment;
     String title;
     String cont_writer;
     String hosts;
-    //String username;
+    String username;
     String categories;
     // List<CateData>
     String period;
@@ -20,6 +21,113 @@ public class ContestData {
     int clips;
     int views;
 
+    public int getRecruitment() {
+        return recruitment;
+    }
+
+    public void setRecruitment(int recruitment) {
+        this.recruitment = recruitment;
+    }
+
+    public int getContests_id() {
+        return contests_id;
+    }
+
+    public void setContests_id(int contests_id) {
+        this.contests_id = contests_id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCont_writer() {
+        return cont_writer;
+    }
+
+    public void setCont_writer(String cont_writer) {
+        this.cont_writer = cont_writer;
+    }
+
+    public String getHosts() {
+        return hosts;
+    }
+
+    public void setHosts(String hosts) {
+        this.hosts = hosts;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCategories() {
+        return categories;
+    }
+
+    public void setCategories(String categories) {
+        this.categories = categories;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getPositions() {
+        return positions;
+    }
+
+    public void setPositions(String positions) {
+        this.positions = positions;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
+
+    public int getAppliers() {
+        return appliers;
+    }
+
+    public void setAppliers(int appliers) {
+        this.appliers = appliers;
+    }
+
+    public int getClips() {
+        return clips;
+    }
+
+    public void setClips(int clips) {
+        this.clips = clips;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
 
     public String getTitle()
     { return title; }
